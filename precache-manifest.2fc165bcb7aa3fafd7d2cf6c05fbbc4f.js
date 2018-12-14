45f30fa5200e74b9ef6f840120144a1635226e05
Ryan Blakeman <rblakeman31@gmail.com>
2018-12-13 23:52:23 -0800

    Updates

---
+++ precache-manifest.2fc165bcb7aa3fafd7d2cf6c05fbbc4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38ff7dda290ddda2f701",
+    "url": "/ColorApp/static/js/runtime~main.38ff7dda.js"
+  },
+  {
+    "revision": "3ba471a335379324e418",
+    "url": "/ColorApp/static/js/main.3ba471a3.chunk.js"
+  },
+  {
+    "revision": "473a6a100d38ebe08b4b",
+    "url": "/ColorApp/static/js/1.473a6a10.chunk.js"
+  },
+  {
+    "revision": "3ba471a335379324e418",
+    "url": "/ColorApp/static/css/main.ae5f355a.chunk.css"
+  },
+  {
+    "revision": "545ac9f3bed4d75c95ead50890b9974e",
+    "url": "/ColorApp/index.html"
+  }
+];
