0c0f5241d6fbe076ede42e7723e3736f53f0d47e
Ryan Blakeman <rblakeman31@gmail.com>
2022-08-07 21:18:25 -0700

    fix ts errors

--- src/components/color-square.tsx
+++ src/components/color-square.tsx
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '@material-ui/core/Card';
 
-export default class ColorSquare extends Component {
-    reverse(str) {
+type Props = {
+    color: string;
+    name: string;
+};
+
+export default function ColorSquare(props: Props) {
+    const reverse = (str: string): string => {
         if (str === '') {
             return '';
         }
 
-        return this.reverse(str.substr(1)) + str.charAt(0);
-    }
+        return reverse(str.substr(1)) + str.charAt(0);
+    };
 
-    render() {
-        // let hex = this.props.color.substr(1)
-        // // hex = this.reverse(hex)
-        // hex = hex[4] + hex[5] + hex[2] + hex[3] + hex[0] + hex[1]
-        // hex = '#' + hex
+    // let hex = this.props.color.substr(1)
+    // // hex = this.reverse(hex)
+    // hex = hex[4] + hex[5] + hex[2] + hex[3] + hex[0] + hex[1]
+    // hex = '#' + hex
 
-        return (
-            <Card
-                style={{
-                    width: 250,
-                    height: 100,
-                    color: 'white',
-                    backgroundColor: this.props.color,
-                    textAlign: 'center'
-                }}>
-                {this.props.name}
-            </Card>
-        );
-    }
+    return (
+        <Card
+            style={{
+                width: 250,
+                height: 100,
+                color: 'white',
+                backgroundColor: props.color,
+                textAlign: 'center'
+            }}>
+            {props.name}
+        </Card>
+    );
 }
--- src/components/input-field.tsx
+++ src/components/input-field.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import ColorIcon from '@material-ui/icons/ColorLensOutlined';
@@ -6,21 +6,21 @@ import ColorIcon from '@material-ui/icons/ColorLensOutlined';
 const styles = {
     container: {
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'row' as 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap' as 'wrap'
     },
     info: {
         display: 'inline',
-        flexDirection: 'column',
+        flexDirection: 'column' as 'column',
         minWidth: '242px',
         padding: 20
     },
     hrefs: {
         textDecoration: 'none',
         color: 'blue',
-        whiteSpace: 'nowrap'
+        whiteSpace: 'nowrap' as 'nowrap'
     },
     textField: {
         margin: 10
@@ -30,58 +30,58 @@ const styles = {
     }
 };
 
-export default class InputField extends Component {
-    state = {
-        val: '12ab5f'
+type Props = {
+    onInputSubmit: (value: string) => void;
+};
+
+export default function InputField(props: Props) {
+    const [val, setVal] = useState('12ab5f');
+
+    const inputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setVal(event.target.value);
     };
 
-    inputChange = (event) => {
-        this.setState({ val: event.target.value });
+    const submitChange = () => {
+        props.onInputSubmit(val);
     };
 
-    submitChange = (e) => {
-        this.props.onInputSubmit(this.state.val);
-    };
-
-    render() {
-        return (
-            <div style={styles.container}>
-                <ColorIcon
-                    style={{
-                        width: '10%',
-                        height: '10%',
-                        minWidth: '75px',
-                        minHeight: '75px'
-                    }} />
-                <div style={styles.info}>
-                    {'Colors from: '}
-                    <a style={styles.hrefs} href="http://www.thecolorapi.com/">
-                        The Color API
-                    </a>
-                    <br />
-                    {'Pictures from: '}
-                    <a style={styles.hrefs} href="https://unsplash.com/">
-                        Unsplash API
-                    </a>
-                    <br />
-                    (Max requests of ~50 pictures/hr)
-                </div>
-                <TextField
-                    required={true}
-                    id="outlined-required"
-                    label="Hex Value"
-                    style={styles.textField}
-                    margin="normal"
-                    variant="outlined"
-                    onChange={this.inputChange}
-                    value={this.state.val} />
-                <Button
-                    variant="contained"
-                    onClick={this.submitChange}
-                    style={styles.submitButton}>
-                Submit
-                </Button>
+    return (
+        <div style={styles.container}>
+            <ColorIcon
+                style={{
+                    width: '10%',
+                    height: '10%',
+                    minWidth: '75px',
+                    minHeight: '75px'
+                }} />
+            <div style={styles.info}>
+                {'Colors from: '}
+                <a style={styles.hrefs} href="http://www.thecolorapi.com/">
+                    The Color API
+                </a>
+                <br />
+                {'Pictures from: '}
+                <a style={styles.hrefs} href="https://unsplash.com/">
+                    Unsplash API
+                </a>
+                <br />
+                (Max requests of ~50 pictures/hr)
             </div>
-        );
-    }
+            <TextField
+                required={true}
+                id="outlined-required"
+                label="Hex Value"
+                style={styles.textField}
+                margin="normal"
+                variant="outlined"
+                onChange={inputChange}
+                value={val} />
+            <Button
+                variant="contained"
+                onClick={submitChange}
+                style={styles.submitButton}>
+            Submit
+            </Button>
+        </div>
+    );
 }
